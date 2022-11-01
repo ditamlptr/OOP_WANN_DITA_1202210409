c39f2aa651061dac57550f5306f16a6cc1e4569f
ditamlptr <116327930+ditamlptr@users.noreply.github.com>
2022-11-01 16:12:35 +0700

    Add files via upload

---
+++ MODUL1_DITA/src/MODUL1_DITA/Menu.java
@@ -0,0 +1,28 @@
+package MODUL1;
+
+public class Menu {
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+    String name;
+    String category;
+    Int price;
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+    public void setCategory(String category){
+        this.cataegory = category;
+
+    }
+
+    public void setPrice(Int price){
+        this.price = price;
+
+    }
+
+
+
+
+}
---
+++ MODUL1_DITA/src/MODUL1_DITA/User.java
@@ -0,0 +1,20 @@
+package MODUL1;
+
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String name;
+    int noHandphone;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNoHandphone(int noHandphone){
+        this.noHandphone = noHandphone;
+    }
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
